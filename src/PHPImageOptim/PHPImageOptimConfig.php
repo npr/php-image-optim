a967d608ffed6becbd2a91ded51fdb82e76eaf3b
DanKottke <dkottke@npr.org>
2014-11-20 15:20:48 -0500

    added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng added some helpful comments and the i0 shell param for optipng

--- src/PHPImageOptim/PHPImageOptimConfig.php
+++ src/PHPImageOptim/PHPImageOptimConfig.php
@@ -11,14 +11,19 @@ class PHPImageOptimConfig
 {
     public static $config = [
 
+        // compression libraries and their configurations
         "libs" => [
+            // http://optipng.sourceforge.net/optipng-0.7.5.man.pdf
             "OptiPng" => [
                 "binary"    => "/usr/local/bin/optipng",
-                "options"   => ["-o2"],
+                // o2 = optimization level. 2 seemed to give the best bang for the buck in terms of reduction/time
+                // i0 = interlacing option. 0 turns interlacing off
+                "options"   => ["-o2", "-i0"],
                 "version"   => "--version"
             ]
         ],
 
+        // defines which libraries are applied to which extensions
         "settings" => [
             "png" => [
                 "OptiPng"
